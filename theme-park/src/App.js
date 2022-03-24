6b576f689fe5fb2147f1828ce11eb745416202a7
haubaustin <haubaustin@gmail.com>
2022-03-23 18:29:12 -0700

    Router

--- theme-park/src/App.js
+++ theme-park/src/App.js
@@ -1,17 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header'
 import Main from './components/Main'
+import { Route, Routes, } from 'react-router-dom';
+import About from './components/About.js'
+
+
+
 
 function App() {
 
   return (
     <div className="App">
-      <Header className="header" />
-    
-      <Main />
-      
+    <Header />
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='about' element={<About />} />
+      </Routes> 
     <Footer />
     </div>
   );
